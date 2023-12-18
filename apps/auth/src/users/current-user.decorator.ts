b3658fce8fe76870fcf7696fb85961af070a278c
hongxuli <34845838+hongxuli@users.noreply.github.com>
2023-12-17 23:27:00 -0600

    local strategy

---
+++ apps/auth/src/users/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { UserDocument } from './models/user.schema';
+
+const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
+  const request = context.switchToHttp().getRequest();
+  return request.user;
+};
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) =>
+    getCurrentUserByContext(context),
+);
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   create(createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: bcrypt.hashSync(createUserDto.password, 10),
+    });
+  }
+
+  async verifyUser(email: string, password: string): Promise<any> {
+    const user = await this.usersRepository.findOne({ email });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return user;
   }
 }
